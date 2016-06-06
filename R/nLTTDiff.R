e2d520cd684d354b8acc504cf286a697a32fc896
Thijs Janzen <thijsjanzen@gmail.com>
2016-06-06 12:03:45 +0000

    version 1.2.1

--- R/nLTTDiff.R
+++ R/nLTTDiff.R
@@ -1,135 +1,191 @@
-nLTTstat <- function(tree1, tree2, distanceMethod = "abs")
-{
- 	diff <- -10;
- 	if(distanceMethod == "abs") diff = normLTTdiffABS(tree1,tree2);
- 	if(distanceMethod == "squ") diff = normLTTdiffSQ(tree1,tree2);
- 	 	
- 	if(diff < 0) {cat("chosen unknown distance method!\n"); flush.console();}
- 	return(diff);	
-}
+################################################################################
+# 
+# @brief Calculates the exact, difference between the lineage through time curves of tree1 & tree2 (normalized in time and for the number of lineages)
+#
+# @date Last modified: 2016-05-20
+# @author Thijs Janzen
+# @since 2016-05-20, version 1.2.1
+#
+# @param    tree1                  phylo      First phylogenetic tree
+# @param    tree2                  phylo      Second phylogenetic tree
+# @param    distance_method        string     absolute, or squared distance?
+# @return                          scalar     normalized Lineage-Through-Time difference between tree1 & tree2
+#
+################################################################################
 
-nLTTstat_exact <- function(tree1, tree2, distanceMethod = "abs")
-{
- 	diff <- -10;
- 	if(distanceMethod == "abs") diff = normLTTdiffexactABS(tree1,tree2);
- 	if(distanceMethod == "squ") diff = normLTTdiffexactSQ(tree1,tree2);
- 	 	
- 	if(diff < 0) {cat("chosen unknown distance method!\n"); flush.console();}
- 	return(diff);	
-}
+nltt_diff_exact <- function(tree1, tree2, distance_method = "abs")  {
+  #branching times of tree1, including the present time (0)
+  b_times <- c(-1 * rev(sort(ape::branching.times(tree1))), 0);
 
+  #the number of lineages, we assume that the
+  # first branching time indicates the crown age.
+  lineages <- c(2:length(b_times), length(b_times));
 
+  b_times_N <- 1 - b_times / min(b_times); #normalize branching times
+  lineages_N <- lineages / max(lineages);  #normalize lineages
 
-normLTTdiffexactABS <- function(tree1,tree2)
-{
-	b_times <- c(-1 * rev(sort(branching.times(tree1))),0);
-  	lineages <- c(2:length(b_times),length(b_times));
-  	b_times_N <- 1 - b_times / min(b_times); #normalize branching times  
-  	lineages_N <- lineages / max(lineages);  #normalize lineages  	
-	
-	b_times2 <- c(-1 * rev(sort(branching.times(tree2))),0);
-    lineages2 <- c(2:length(b_times2),length(b_times2));  
-    b_times2_N <- 1 - b_times2 / min(b_times2); #normalize branching times  
-    lineages2_N <- lineages2 / max(lineages2);  #normalize lineages  
-	
-	allBtimes <- unique(sort(c(b_times_N,b_times2_N)));
-	diff <- 0;
-	for(k in 2:length(allBtimes))
-	{
-			tim <- allBtimes[k];
-			index1 <- max(which(b_times_N <= tim));
-			index2 <- max(which(b_times2_N <= tim));
-			lins1 <- lineages_N[index1];
-			lins2 <- lineages2_N[index2];
-			dt <- allBtimes[k] - allBtimes[k-1]
-			diff <- diff + dt * abs(lins1-lins2);
-	}
-	return(diff);
-}
+  b_times2 <- c(-1 * rev(sort(ape::branching.times(tree2))), 0);
+  lineages2 <- c(2:length(b_times2), length(b_times2));
+  b_times2_N <- 1 - b_times2 / min(b_times2); #normalize branching times
+  lineages2_N <- lineages2 / max(lineages2);  #normalize lineages
 
-normLTTdiffexactSQ <- function(tree1,tree2)
-{
-	b_times <- c(-1 * rev(sort(branching.times(tree1))),0);
-  	lineages <- c(2:length(b_times),length(b_times));
-  	b_times_N <- 1 - b_times / min(b_times); #normalize branching times  
-  	lineages_N <- lineages / max(lineages);  #normalize lineages  	
-	
-	b_times2 <- c(-1 * rev(sort(branching.times(tree2))),0);
-    lineages2 <- c(2:length(b_times2),length(b_times2));  
-    b_times2_N <- 1 - b_times2 / min(b_times2); #normalize branching times  
-    lineages2_N <- lineages2 / max(lineages2);  #normalize lineages  
-	
-	allBtimes <- unique(sort(c(b_times_N,b_times2_N)));
-	diff <- 0;
-	for(k in 2:length(allBtimes))
-	{
-			tim <- allBtimes[k];
-			index1 <- max(which(b_times_N <= tim));
-			index2 <- max(which(b_times2_N <= tim));
-			lins1 <- lineages_N[index1];
-			lins2 <- lineages2_N[index2];
-			dt <- allBtimes[k] - allBtimes[k-1]
-			diff <- diff + dt * (lins1-lins2)*(lins1-lins2);
-	}
-	return(diff);
-}
+  #make a list of all branching times, and remove duplicates
+  all_b_times <- unique(sort(c(b_times_N, b_times2_N)));
+  diff <- 0;
+  #iterate through all branching times
+  for (k in 2:length( all_b_times)) {
+      tim <- all_b_times[k];
+      #find the index of the first branching time
+      #that is up to the focal branching time
+      index1 <- max(which(b_times_N < tim));
+      index2 <- max(which(b_times2_N < tim));  #same for the other tree
 
+      #find the number of lineages at time "tim" for tree 1
+      lins1 <- lineages_N[max(index1, 1)];
+      #find the number of lineages at time "tim" for tree 2
+      lins2 <- lineages2_N[max(index2, 1)];
 
-
-
-
-
-normLTTdiffABS <- function(tree1, tree2) {
-
-  b_times <- c(-1 * rev(sort(branching.times(tree1))),0);
-  lineages <- c(2:length(b_times),length(b_times));
-  b_times_N <- 1 - b_times / min(b_times); #normalize branching times  
-  lineages_N <- lineages / max(lineages);  #normalize lineages  
-  ltt1 <- approxfun(b_times_N,lineages_N,method="constant");
-
-  b_times2 <- c(-1 * rev(sort(branching.times(tree2))),0);
-  lineages2 <- c(2:length(b_times2),length(b_times2));  
-  b_times2_N <- 1 - b_times2 / min(b_times2); #normalize branching times  
-  lineages2_N <- lineages2 / max(lineages2);  #normalize lineages  
-  ltt2 <- approxfun(b_times2_N,lineages2_N,method="constant");
-
-  f <- function(t,x,p) { #function f is the absolute difference in time t: 0 <= t < 1
-       output <- abs( ltt1(t) - ltt2(t));
-       return(list(output));
+      #the amount of time that this difference persisted
+      dt <- all_b_times[k] - all_b_times[k - 1]
+      if (distance_method == "abs") {
+        diff <- diff + dt * abs( lins1 - lins2);     #update the difference
+      }
+      if (distance_method == "squ")  {
+        diff <- diff + dt * ( lins1 - lins2) * ( lins1 - lins2);
+      }
   }
-  
-  times <- (0:100)/100;
-  int_1 <- lsoda(0,times,func=f,tcrit=c(1)); #integrate over t: 0 < t < 1, notice tcrit=0 indicating t should never be larger than 0.
-  total_area <- int_1[length(times),2]
-  
-  return(total_area);
+  return ( diff);
 }
+################################################################################
+# 
+# @brief Calculates the exact, difference between the lineage through time curves of tree1 & tree2 (normalized in time and for the number of lineages)
+#
+# @date Last modified: 2016-05-20
+# @author Thijs Janzen
+# @since 2016-05-20, version 1.2.1
+#
+# @param    tree1                  phylo      First phylogenetic tree
+# @param    tree2                  phylo      Second phylogenetic tree
+# @param    distance_method        string     absolute, or squared distance?
+# @return                          scalar     normalized Lineage-Through-Time difference between tree1 & tree2
+#
+################################################################################
 
-normLTTdiffSQ <- function(tree1, tree2) {
+nltt_diff <- function(tree1, tree2, distance_method = "abs")  {
+  #branching times of tree1, including the present time (0)
+  b_times    <- c(-1 * rev( sort( ape::branching.times( tree1))), 0);
 
-  b_times <- c(-1 * rev(sort(branching.times(tree1))),0);
-  lineages <- c(2:length(b_times),length(b_times));
-  b_times_N <- 1 - b_times / min(b_times); #normalize branching times  
-  lineages_N <- lineages / max(lineages);  #normalize lineages  
-  ltt1 <- approxfun(b_times_N,lineages_N,method="constant");
+  #the number of lineages
+  #we assume that the first branching time indicates the crown age.
+  lineages   <- c( 2:length( b_times), length( b_times));
+  b_times_N  <- 1 - b_times / min( b_times); #normalize branching times
+  lineages_N <- lineages / max( lineages); #normalize lineages
 
-  b_times2 <- c(-1 * rev(sort(branching.times(tree2))),0);
-  lineages2 <- c(2:length(b_times2),length(b_times2));  
-  b_times2_N <- 1 - b_times2 / min(b_times2); #normalize branching times  
-  lineages2_N <- lineages2 / max(lineages2);  #normalize lineages  
-  ltt2 <- approxfun(b_times2_N,lineages2_N,method="constant");
+  #method = constant ensures a step function
+  ltt1       <- approxfun( b_times_N, lineages_N, method = "constant");
 
-  f <- function(t,x,p) { #function f is the absolute difference in time t: 0 <= t < 1
-       output <- ltt1(t) - ltt2(t);
-	   output <- output * output;
-       return(list(output));
+  #branching times of tree2, including the present time (0)
+  b_times2    <- c(-1 * rev( sort( ape::branching.times( tree2))), 0);
+
+  #the number of lineages
+  #we assume that the first branching time indicates the crown age.
+  lineages2   <- c( 2:length( b_times2), length( b_times2));
+
+  b_times2_N  <- 1 - b_times2 / min( b_times2); #normalize branching times
+  lineages2_N <- lineages2 / max( lineages2);  #normalize lineages
+  #method = constant ensures a step function
+  ltt2        <- approxfun( b_times2_N, lineages2_N, method = "constant");
+
+  #function f is the absolute difference in time t: 0 <= t < 1
+  f <- function( t, x, p) {
+       if ( distance_method == "abs" ) {
+         output <- abs( ltt1(t) - ltt2(t) );
+       }
+       if ( distance_method == "squ" ) {
+         output <- (ltt1(t) - ltt2(t)) * (ltt1(t) - ltt2(t))
+       }
+       return( list( output) );
   }
-  
-  times <- (0:100)/100;
-  int_1 <- lsoda(0,times,func=f,tcrit=c(1)); #integrate over t: 0 < t < 1, notice tcrit=0 indicating t should never be larger than 0.
-  total_area <- int_1[length(times),2]
-  
-  return(total_area);
+
+  #evaluation points of the integration function below
+  #more points ensures higher precision.
+  times <- ( 0:100 ) / 100;
+  #integrate over t: 0 < t < 1, notice tcrit=0 indicating t
+  #should never be larger than 0:
+  int_1 <- deSolve::lsoda( 0, times, func = f, tcrit = c( 1 ) );
+  total_area <- int_1[length(times), 2]
+  return( total_area[[1]])
 }
 
+################################################################################
+# 
+# @brief Wrapper to calculate the nLTT statistic
+#
+# @date Last modified: 2015-21-04
+# @author Thijs Janzen
+# @since 2015-21-04, version 1.1
+#
+# @param    tree1                  phylo      First phylogenetic tree
+# @param    tree2                  phylo      Second phylogenetic tree
+# @param    distance_method        string     Method to calculate the difference, either absolute, or squared
+# @return                          scalar     normalized Lineage-Through-Time difference between tree1 & tree2
+#
+################################################################################
 
+nLTTstat <- function( tree1, tree2, distance_method = "abs") {
+  if (!inherits(tree1, "phylo")) {
+    # Just checking
+    stop("nLTT.plot: ",
+      "tree1 must be of class 'phylo', ",
+      "but was of type '", class(tree1), "' instead")
+  }
+  if (!inherits(tree2, "phylo")) {
+    # Just checking
+    stop("nLTT.plot: ",
+      "tree1 must be of class 'phylo', ",
+      "but was of type '", class(tree2), "' instead")
+  }
+
+  if ( distance_method != "abs" && distance_method != "squ") {
+    cat( "chosen unknown distance method!\n" );
+    flush.console();
+  }
+  diff <- nltt_diff( tree1, tree2, distance_method);
+  return (diff);
+}
+
+################################################################################
+# 
+# @brief Wrapper to calculate the nLTT statistic - exact version
+#
+# @date Last modified: 2016-26-04
+# @author Thijs Janzen
+# @since 2016-26-04, version 1.2
+#
+# @param    tree1                  phylo      First phylogenetic tree
+# @param    tree2                  phylo      Second phylogenetic tree
+# @param    distance_method        string     Method to calculate the difference, either absolute, or squared
+# @return                          scalar     normalized Lineage-Through-Time difference between tree1 & tree2
+#
+################################################################################
+
+nLTTstat_exact <- function(tree1, tree2, distance_method = "abs")   {
+  if (!inherits(tree1, "phylo")) {
+    # Just checking
+    stop("nLTT.plot: ",
+      "tree1 must be of class 'phylo', ",
+      "but was of type '", class(tree1), "' instead")
+  }
+  if (!inherits(tree2, "phylo")) {
+    # Just checking
+    stop("nLTT.plot: ",
+      "tree1 must be of class 'phylo', ",
+      "but was of type '", class(tree2), "' instead")
+  }
+  if (distance_method != "abs" && distance_method != "squ") {
+    cat("chosen unknown distance method!\n");
+    flush.console();
+  }
+  diff <- nltt_diff_exact( tree1, tree2, distance_method);
+  return (diff);
+}
--- R/plot_nLTT.R
+++ R/plot_nLTT.R
@@ -1,22 +1,63 @@
-nLTT.plot <- function(phy,xlab="Normalized Time",ylab="Normalized Lineages", ...) {
+################################################################################
+# 
+# @brief plot the normalized nLTT plot of a tree
+#
+# @date Last modified: 2014-20-09
+# @author Thijs Janzen
+# @since 2014-20-09, version 1.0
+#
+# @param    tree1                  phylo      Phylogenetic tree
+# @param    xlab                   string     Label on the x-axis
+# @param    ylab                   string     Label on the y-axis
+# 
+#
+################################################################################
 
- xy <- ltt.plot.coords(phy,backward=TRUE,tol=1e-6);
- xy[,2] <- xy[,2]/max(xy[,2]); #normalize number lineages
- 
- xy[,1] <- xy[,1] + abs(min(xy[,1])); #make sure time runs from 0..T
- xy[,1] <- xy[,1] / max(xy[,1]);      #normalize time
- 
- plot.default(xy, xlab = xlab, ylab = ylab, xaxs = "r", yaxs = "r", 
-					type = "S", ...)
+nltt_plot <- function( phy, xlab = "Normalized Time",
+  ylab = "Normalized Lineages", ...) {
+
+  if (!inherits(phy, "phylo")) {
+    # Stop imposed by ape::ltt.plot.coords
+    stop("nLTT.plot: ",
+      "phylogeny must be of class 'phylo', ",
+      "but was of type '", class(phy), "' instead")
+  }
+
+  #we use the ltt.plot.coords function from the package ape
+  xy <- ape::ltt.plot.coords( phy, backward = TRUE, tol = 1e-6);
+  xy[, 2] <- xy[, 2] / max(xy[, 2]); #normalize number lineages
+
+  xy[, 1] <- xy[, 1] + abs( min( xy[, 1])); #make sure time runs from 0..T
+  xy[, 1] <- xy[, 1] / max( xy[, 1]);      #normalize time
+
+  plot.default(xy, xlab = xlab, ylab = ylab, xaxs = "r", yaxs = "r",
+    type = "S", ...) #type = "S" ensures a stepwise function
 }
-		
-nLTT.lines <- function(phy, ...) {
 
-  xy <- ltt.plot.coords(phy, backward=TRUE, tol=1e-6)
-  xy[,2] <- xy[,2]/max(xy[,2]); #normalize number lineages
- 
-  xy[,1] <- xy[,1] + abs(min(xy[,1])); #make sure time runs from 0..T
-  xy[,1] <- xy[,1] / max(xy[,1]);      #normalize time
+################################################################################
+# 
+# @brief add a nLTT line to an existing nLTT plot
+# @date Last modified: 2014-20-09
+# @author Thijs Janzen
+# @since 2014-20-09, version 1.0
+#
+# @param    tree1                  phylo      Phylogenetic tree
+# 
+#
+################################################################################
+
+nltt_lines <- function(phy, ...) {
+
+  if (!inherits(phy, "phylo")) {
+    # Stop imposed by ape::ltt.plot.coords
+    stop("nLTT.lines: ",
+      "phylogeny must be of class 'phylo', ",
+      "but was of type '", class(phy), "' instead")
+  }
+  xy <- ape::ltt.plot.coords( phy, backward = TRUE, tol = 1e-6)
+  xy[, 2] <- xy[, 2] / max( xy[, 2]); #normalize number lineages
+
+  xy[, 1] <- xy[, 1] + abs( min( xy[, 1])); #make sure time runs from 0..T
+  xy[, 1] <- xy[, 1] / max( xy[, 1]);      #normalize time
   lines(xy, type = "S", ...)
 }
-		
